c6e862061946913924be05ff215919ad27a4879b
AliChegini <ali.c.fullstack@gmail.com>
2023-03-12 13:46:06 +0100

    small progress

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-
-let ClickCounter =  {
-    template: '<button @click="count++">{{count}}</button>',
-    data() {
-        return {
-            count:0
-        }
-    }
-}
-
-
 let InputField = {
     props: {
         label: String,
@@ -16,17 +5,43 @@ let InputField = {
     template: `
         <div>
             <label>{{label}}</label>
-            <input type="text">
+            <input type="text" v-model="inputValue" @input="updateValue" @keyup.enter="updateValue">
         </div>
     `,
-}
+    data() {
+        return  {
+            inputValue: ''
+        };
+    },
+    methods: {
+        updateValue() {
+            this.$emit('update:count', this.inputValue);
+        }
+    }
+};
 
+let ClickCounter =  {
+    components: {
+        'input-field': InputField,
+    },
+    template: `
+        <div>
+            <button @click="count++">{{count}}</button>
+            <input-field label="Enter a value" v-bind:count.sync="count"></input-field>
+        </div>
+    `,
+    data() {
+        return {
+            count:0
+        }
+    }
+}
 
 let app = Vue.createApp({
     components: {
         'click-counter': ClickCounter,
-        'input-field': InputField,
       },
 })
 
 .mount("#app");
+
